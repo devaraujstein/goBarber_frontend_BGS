06be65c0a7b9b32e4da58b1f6035fbedc4d35de5
devaraujstein <andreearaujo98@gmail.com>
2020-10-12 15:55:14 -0300

    Validando Cadastro e exibindo erros no input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { Form } from '@unform/web';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -10,9 +11,24 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: any): void {
-    console.log(data);
-  }
+  /* eslint-disable @typescript-eslint/ban-types */
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name required'),
+        email: Yup.string()
+          .required('Email required')
+          .email('Type a valid email'),
+        password: Yup.string().min(6, 'Minimum 6 digits'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
